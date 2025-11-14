fdcc08b0d53dac8a2f09ed516c6e81e2f2501732
vijaymandal6708 <vijaymandal6708@gmail.com>
2025-11-14 08:44:37 +0530

    14/11/25

--- task-management-system/server/index.js
+++ task-management-system/server/index.js
@@ -6,18 +6,17 @@ require("dotenv").config();
 const AdminRoute = require("./routes/adminRoute");
 const EmployeeRoute = require("./routes/employeeRoute");
 
+app.use(cors({
+    origin: 'https://vijaytaskfrontt.onrender.com',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
+
 mongoose.connect(process.env.DBCONN).then(()=>{
      console.log("DB Succesfully Connected!");
 });
 app.use(express.json());
 
-// app.use(cors({
-//     origin: 'https://vijaytaskfrontt.onrender.com',
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   credentials: true,
-// }));
-
-app.use(cors());
 
 app.use("/admin", AdminRoute);
 app.use("/employee", EmployeeRoute);
