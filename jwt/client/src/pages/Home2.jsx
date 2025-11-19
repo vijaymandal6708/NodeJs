b7499e38e7e6ed238a9bde008bd0068f894894da
vijaymandal6708 <vijaymandal6708@gmail.com>
2025-11-19 18:21:54 +0530

    19/11/25

--- jwt/client/src/pages/Home2.jsx
+++ jwt/client/src/pages/Home2.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Home2 = () => {
+    const [file, setFile] = useState("");
+
+    const handleFile =(e)=>{
+        console.log(e.target.files[0]);
+        setFile(e.target.files[0]);
+    };
+
+    const handleUpload =async()=>{
+        const formdata=new FormData();
+        formdata.append("myfile", file);
+        const response =await axios.post("http://localhost:9000/upload", formdata);
+        console.log(response.data);
+    }
+
   return (
-
-
-    
     <div>
       <h1>Upload File using Multer</h1>
-      Upload File: <input type="file" onChange={handleImage}/>
-      <button onClick={handleUpload}></button>
+      Upload File: <input type="file" onChange={handleFile}/>
+      <button onClick={handleUpload}>Upload</button>
     </div>
   )
 }
--- jwt/server/index.js
+++ jwt/server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const studentRoute = require("./routes/studentRoute");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const multer = require("multer");
 
 mongoose.connect("mongodb://localhost:27017/jwt").then(()=>{
     console.log("mongodb connected")
@@ -12,6 +13,22 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/students", studentRoute);
+
+const storage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null, 'uploads/');
+    },
+    filename: (req,file,cb)=>{
+        cb(null, `${Date.now()}-${file.originalname}`);
+    }
+});
+
+const upload=multer({storage:storage});
+
+app.post("/upload", upload.single("myfile"), (req,res)=>{
+    console.log(req.file.filename);
+    res.send("File Uploaded!!!");
+})
 
 app.listen(9000, ()=>{
     console.log("server is running on port 9000!");
