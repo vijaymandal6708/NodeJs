4b2679b38df49e4cd7d2fe2c85b2162fec4b3220
vijaymandal6708 <vijaymandal6708@gmail.com>
2025-12-24 08:15:07 +0530

    product detail page

---
+++ 00NodeJs/newpractice/NewPractice/client/src/components/Insert.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Insert = () => {
+  return (
+    <div>
+      <h4>Add student data</h4>
+      Enter Name : <input type="text" /> <br />
+      Enter Roll no : <input type="text" /> <br />
+      Enter City : <input type="text" /> <br />
+      Enter Fess : <input type="number" /> <br />
+    </div>
+  )
+}
+
+export default Insert
--- 00_Final_Project/client/src/pages/ProductDetail.jsx
+++ 00_Final_Project/client/src/pages/ProductDetail.jsx
@@ -3,20 +3,21 @@ import React, { useEffect, useState } from "react";
 
 const ProductDetail = () => {
   const [data, setData] = useState({});
+  const [activeImage, setActiveImage] = useState("");
 
   const loadData = async () => {
     const response = await axios.get(
       `${import.meta.env.VITE_BACKENDURL}/product/product-detail`
     );
     setData(response.data);
-    console.log(response.data);
+    setActiveImage(response.data.defaultImage);
   };
 
   useEffect(() => {
     loadData();
   }, []);
 
-  const starRating = parseFloat(data.starRating)||0;
+  const starRating = parseFloat(data.starRating) || 0;
 
   return (
     <div
@@ -38,18 +39,29 @@ const ProductDetail = () => {
           style={{
             height: "520px",
             width: "37vw",
-            border: "1px solid black",
             display: "flex",
           }}
         >
-          <div
-            className="thumb"
-            style={{ height: "520px", width: "22%", border: "1px solid black" }}
-          >
-            {/* <img src="" alt="" /> */}
-            {/* <img src="" alt="" /> */}
-            {/* <img src="" alt="" /> */}
-            {/* <img src="" alt="" /> */}
+          <div className="thumb" style={{ height: "520px", width: "22%",paddingTop:"20px",paddingRight:"15px",paddingLeft:"15px"}}>
+            {data.images?.map((img, index) => (
+              <img
+                key={index}
+                src={img}
+                onClick={() => setActiveImage(img)}
+                style={{
+                  width: "80px",
+                  height: "80px",
+                  objectFit: "cover",
+                  marginBottom: "20px",
+                  cursor: "pointer",
+                  border:
+                    activeImage === img
+                      ? "2px solid rgba(95,35,198,1)"
+                      : "1px solid lightgrey",
+                  borderRadius: "8px",
+                }}
+              />
+            ))}
           </div>
           <div
             className="main-image"
@@ -57,7 +69,7 @@ const ProductDetail = () => {
               height: "519px",
               width: "78%",
               padding: "50px 20px",
-              background: "lightgrey",
+              background: "rgba(0, 0, 0, 0.07)",
               border: "8px solid white",
               boxSizing: "border-box",
               borderRadius: "30px",
@@ -67,7 +79,7 @@ const ProductDetail = () => {
             }}
           >
             <img
-              src="/asus-rog-strix01.jpg"
+              src={activeImage}
               style={{
                 maxHeight: "100%",
                 maxWidth: "100%",
@@ -81,9 +93,8 @@ const ProductDetail = () => {
           style={{
             height: "520px",
             width: "48vw",
-            border: "1px solid black",
-            paddingTop: "50px",
-            paddingBottom: "50px",
+            paddingTop: "30px",
+            paddingBottom: "30px",
             paddingLeft: "60px",
             paddingRight: "10px",
           }}
@@ -91,11 +102,158 @@ const ProductDetail = () => {
           <h2 style={{ fontWeight: "bolder", fontSize: "34px" }}>
             {data.name}
           </h2>
-          <div className="rating">
+          <div
+            className="rating"
+            style={{
+              color: "rgba(237, 184, 48, 1)",
+              fontSize: "18px",
+              margin: "15px",
+            }}
+          >
             {"★".repeat(Math.floor(starRating))}
             {starRating % 1 >= 0.5 && "⯪"}
-            {"☆".repeat(5 - Math.floor(starRating) - (starRating % 1 >= 0.5 ? 1 : 0))}
-            <span style={{marginLeft:"15px"}}>{starRating} stars</span>
+            {"☆".repeat(
+              5 - Math.floor(starRating) - (starRating % 1 >= 0.5 ? 1 : 0)
+            )}
+            <span
+              style={{ marginLeft: "15px", color: "black", fontSize: "14px" }}
+            >
+              {starRating} stars
+            </span>
+          </div>
+          <div
+            className="price"
+            style={{
+              height: "30px",
+              width: "500px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "left",
+              marginBottom: "25px",
+            }}
+          >
+            <span
+              style={{
+                fontSize: "22px",
+                fontWeight: "700",
+                color: "rgba(95, 35, 198, 1)",
+              }}
+            >
+              ₹{data.price}
+            </span>
+            <span
+              style={{
+                marginLeft: "20px",
+                fontSize: "17px",
+                textDecoration: "line-through",
+              }}
+            >
+              ₹{data.MRP}
+            </span>
+            <span
+              style={{
+                marginLeft: "20px",
+                fontSize: "15px",
+                color: "rgba(34, 126, 66, 1)",
+                fontWeight: "bolder",
+              }}
+            >
+              30% OFF
+            </span>
+          </div>
+          <div
+            className="description"
+            style={{ wordSpacing: "0.3px", marginBottom: "20px" }}
+          >
+            {data.description}
+          </div>
+          <p
+            style={{
+              color: "rgba(34, 126, 66, 1)",
+              fontSize: "14px",
+              fontWeight: "600",
+              marginBottom: "5px",
+              marginLeft: "5px",
+            }}
+          >
+            ✔ In stock • Free delivery • Easy returns
+          </p>
+          <p
+            style={{
+              color: "rgba(80, 80, 80, 1)",
+              fontSize: "14px",
+              marginLeft: "3px",
+              marginBottom: "25px",
+            }}
+          >
+            🛡 1-year warranty • Secure payments • 7-day replacement
+          </p>
+
+          <div
+            className="quantity"
+            style={{ marginBottom: "30px", marginLeft: "20px" }}
+          >
+            <button
+              style={{
+                padding: "3px 13px",
+                marginRight: "15px",
+                borderRadius: "5px",
+                paddingBottom: "5px",
+                border: "1px solid lightgrey",
+              }}
+            >
+              -
+            </button>
+            <span>1</span>
+            <button
+              style={{
+                padding: "3px 11px",
+                marginLeft: "15px",
+                borderRadius: "5px",
+                paddingBottom: "5px",
+                border: "1px solid lightgrey",
+              }}
+            >
+              +
+            </button>
+          </div>
+          <div
+            className="action-buttons"
+            style={{
+              display: "flex",
+              height: "40px",
+              width: "640px",
+              gap: "30px",
+            }}
+          >
+            <div
+              className="add-to-cart"
+              style={{
+                height: "40px",
+                width: "290px",
+                border: "1px solid black",
+                borderRadius: "11px",
+                color: "white",
+                background: "rgba(95,35,198,1)",
+                padding: "6px 100px",
+              }}
+            >
+              Add to Cart
+            </div>
+            <div
+              className="add-to-cart"
+              style={{
+                height: "40px",
+                width: "290px",
+                border: "1px solid black",
+                borderRadius: "11px",
+                color: "white",
+                background: "rgba(7, 2, 16, 1)",
+                padding: "6px 112px",
+              }}
+            >
+              Buy Now
+            </div>
           </div>
         </div>
       </div>
